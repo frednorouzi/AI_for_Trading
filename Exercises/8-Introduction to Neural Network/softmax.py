e4ae58673143684764441c9b871a5a717dae6518
Fred Norouzi <35445151+frednorouzi@users.noreply.github.com>
2019-08-13 22:34:26 -0700

    Add files via upload

---
+++ Exercises/8-Introduction to Neural Network/softmax.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  2 10:49:58 2019
+
+@author: fred
+"""
+
+import numpy as np
+
+# Write a function that takes as input a list of numbers, and returns
+# the list of values given by the softmax function.
+def softmax(L):
+    expL = np.exp(L)
+    sumExpL = sum(expL)
+    result = []
+    for i in expL:
+        result.append(i*1.0/sumExpL)
+    return result
+    
+    # Note: The function np.divide can also be used here, as follows:
+    # def softmax(L):
+    #     expL = np.exp(L)
+    #     return np.divide (expL, expL.sum())
